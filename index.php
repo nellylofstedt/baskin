2b45d65e93dcbfbd2da81bec14b96026dec406a2
nellylofstedt <31853571+nellylofstedt@users.noreply.github.com>
2019-03-27 13:26:09 +0100

    Börjat med html och lite sass

--- index.php
+++ index.php
@@ -7,6 +7,43 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Det funkar!</h1>
+    <div class="kontainer">
+        <header>
+            <h1>Baskin</h1>
+            <nav>
+                <a class="button" href="#">Logga in</a>
+                <a class="button" href="#">Registrera</a>
+                <a href="#">Home</a>
+                <a href="#">Meny</a>
+                <a href="#">Skapa konto</a>
+            </nav>
+        </header>
+        <main>
+            <section>  
+                <form action="">
+                    <input type="radio" name="food" value="pizza"> Pizza
+                    <input type="radio" name="food" value="sushi"> Sushi
+                    <input type="radio" name="food" value="pasta"> Pasta
+                    <input type="radio" name="food" value="husmanskost"> Husmanskost
+                    <input name="search" type="text">
+                </form>
+                <img src="./images/921b757aca6652942ef75591c8170f0e.png" alt="">
+            </section>
+            <section>  
+                <h1>Mamma Mia</h1>
+                <h2>Pizza</h2>
+                <h3>odir so sljd rslie flei woei woed jwoued woeui wo</h3>
+                <h2>Pasta</h2>
+                <h3>odir so sljd rslie flei woei woed jwoued woeui wo</h3>
+                <button>Meny</button>
+                <img src="./images/NoPath.png">
+                <img src="./images/NoPath-1.png">
+                <img src="./images/NoPath-2.png">
+                <img src="./images/NoPath-3.png">
+                <img src="./images/NoPath-4.png">
+            </section>
+
+        </main>
+    </div>
 </body>
 </html>
