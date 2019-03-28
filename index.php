5e5acc1753605bf4d4981b563704ed02e21693ab
nellylofstedt <31853571+nellylofstedt@users.noreply.github.com>
2019-03-28 14:54:41 +0100

    lärde mig saker

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Baskin</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <div class="kontainer">
@@ -13,28 +13,22 @@
             <nav>
                 <a class="button" href="#">Logga in</a>
                 <a class="button" href="#">Registrera</a>
-                <a href="#">Home</a>
-                <a href="#">Meny</a>
-                <a href="#">Skapa konto</a>
             </nav>
         </header>
         <main>
-            <section>  
+            <section class="karta">  
                 <form action="">
                     <input type="radio" name="food" value="pizza"> Pizza
                     <input type="radio" name="food" value="sushi"> Sushi
                     <input type="radio" name="food" value="pasta"> Pasta
                     <input type="radio" name="food" value="husmanskost"> Husmanskost
-                    <input name="search" type="text">
+                    <input name="search" type="text" placeholder="Search...">
                 </form>
                 <img src="./images/921b757aca6652942ef75591c8170f0e.png" alt="">
             </section>
-            <section>  
-                <h1>Mamma Mia</h1>
-                <h2>Pizza</h2>
-                <h3>odir so sljd rslie flei woei woed jwoued woeui wo</h3>
-                <h2>Pasta</h2>
-                <h3>odir so sljd rslie flei woei woed jwoued woeui wo</h3>
+            <section class="info">  
+                <h2>Mamma Mia</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fugit voluptate natus ducimus tempore beatae deserunt numquam laudantium, eveniet quia nam iste officia magni nesciunt doloremque, esse molestiae, nisi veritatis?</p>
                 <button>Meny</button>
                 <img src="./images/NoPath.png">
                 <img src="./images/NoPath-1.png">
