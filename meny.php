9571aaa919a812ba7d6e2c905726b4cf32bc224c
nellylofstedt <31853571+nellylofstedt@users.noreply.github.com>
2019-03-29 13:20:46 +0100

    kodat typ

---
+++ meny.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Baskin</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <header>
+            <h1>Baskin</h1>
+            <nav>
+                <a class="button" href="#">Logga in</a>
+                <a class="button" href="#">Registrera</a>
+            </nav>
+        </header>
+        <main>
+            <section class="bild">  
+                <img src="./images/bg_2.png" alt="">
+            </section>
+            <section class="info">  
+                <h2>Mamma Mia</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fugit voluptate natus ducimus tempore beatae deserunt numquam laudantium, eveniet quia nam iste officia magni nesciunt doloremque, esse molestiae, nisi veritatis?</p>
+                <button>Meny</button>
+                <img src="./images/NoPath.png">
+                <img src="./images/NoPath-1.png">
+                <img src="./images/NoPath-2.png">
+                <img src="./images/NoPath-3.png">
+                <img src="./images/NoPath-4.png">
+            </section>
+
+        </main>
+    </div>
+</body>
+</html>
